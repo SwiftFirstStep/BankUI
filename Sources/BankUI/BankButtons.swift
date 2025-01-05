b0802b3fbb2b383a96108733ef3bad822839a08c
Vijay Pokuri <pokuri.vijay@gmail.com>
2025-01-05 21:56:38 +0530

    0.0.1

---
+++ Sources/BankUI/BankButtons.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+@available(macOS 10.15.0, *)
+
+public struct BankButtons {
+
+    public static func primaryButton(text: String, action: @escaping () -> Void) -> some View {
+        PrimaryButton(text: text, action: action)
+    }
+    
+    public static func secondaryButton(text: String, action: @escaping () -> Void) -> some View {
+        SecondaryButton(text: text, action: action)
+    }
+    
+    public static func outlineButton(text: String, action: @escaping () -> Void) -> some View {
+        OutlineButton(text: text, action: action)
+    }
+    
+    public static func iconButton(icon: Image,  action: @escaping () -> Void) -> some View {
+        IconButton(icon: icon, action: action)
+    }
+}
--- Sources/BankUI/BankUI.swift
+++ Sources/BankUI/BankUI.swift
@@ -1,6 +1,1 @@
-public struct BankUI {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
+//
